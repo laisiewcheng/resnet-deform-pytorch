7c38c04bfd8a4c658ddd4056905b32dee97b25de
laisiewcheng <laisiewcheng@hotmail.com>
2021-03-30 23:46:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ parser.add_argument('--resume', default='', type=str, metavar='PATH',
 parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 
-parser.add_argument('-b', '--batch-size', default=16, type=int,
+parser.add_argument('-b', '--batch-size', default=8, type=int,
                     metavar='N', help='mini-batch size (default: 128)')
 
 parser.add_argument('--print-freq', '-p', default=50, type=int,
